6abe6718c216f2ae346896e68d9680641bfd9aaf
trevorjtclarke <trevorjtclarke@gmail.com>
2021-10-21 10:00:23 -0700

    update council

--- dao/dao_deploy_mainnet.sh
+++ dao/dao_deploy_mainnet.sh
@@ -7,7 +7,8 @@ DAO_ACCOUNT=$DAO_NAME.$DAO_ROOT_ACC
 # export NEAR_ENV=testnet
 export NEAR_ENV=mainnet
 
-COUNCIL='["croncat.'$DAO_ROOT_ACC'", "cheddar.'$DAO_ROOT_ACC'", "codame.'$DAO_ROOT_ACC'", "ref-finance.'$DAO_ROOT_ACC'", "marmaj.'$DAO_ROOT_ACC'", "openshards.'$DAO_ROOT_ACC'", "collabs.'$DAO_ROOT_ACC'", "auroradao.'$DAO_ROOT_ACC'", "move.'$DAO_ROOT_ACC'", "creatives.'$DAO_ROOT_ACC'", "growth-guild.'$DAO_ROOT_ACC'", "pixeltoken.'$DAO_ROOT_ACC'"]'
+# COUNCIL='["croncat.'$DAO_ROOT_ACC'", "dev.'$DAO_ROOT_ACC'", "community.'$DAO_ROOT_ACC'", "cheddar.'$DAO_ROOT_ACC'", "codame.'$DAO_ROOT_ACC'", "ref-finance.'$DAO_ROOT_ACC'", "marmaj.'$DAO_ROOT_ACC'", "openshards.'$DAO_ROOT_ACC'", "collabs.'$DAO_ROOT_ACC'", "auroradao.'$DAO_ROOT_ACC'", "move.'$DAO_ROOT_ACC'", "creatives.'$DAO_ROOT_ACC'", "growth-guild.'$DAO_ROOT_ACC'", "pixeltoken.'$DAO_ROOT_ACC'"]'
+COUNCIL='["croncat.'$DAO_ROOT_ACC'", "dev.'$DAO_ROOT_ACC'", "community.'$DAO_ROOT_ACC'"]'
 
 #Args for creating DAO in sputnik-factory
 ARGS=`echo "{\"config\":  {\"name\": \"$DAO_NAME\", \"purpose\": \"An interdaomentional space station connecting Governauts reaching for the stars with Astro\", \"metadata\":\"\"}, \"policy\": $COUNCIL}" | base64`
